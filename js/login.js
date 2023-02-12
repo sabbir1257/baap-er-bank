78e8288b44729dcf4d7ea9de50fd852c83807b72
sabbir1257 <masabbir1257@gmail.com>
2023-02-13 02:33:59 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,12 @@
+// step-1: add click event handler with the submit button
+document.getElementById('btn-submit').addEventListener('click', function(){
+    // step-2: get the email address inside the email input field
+    // always remember to use .value to get text from an input field
+    const emailFild = document.getElementById('user-email'); 
+    const email = emailFild.value;
+    // step-3: get password
+    // 3.a: set id on the html
+    // 3.b: get the element
+    // 3.c: get the value from the element
+    
+})
