2e9831206275fc1c3dbbebe7a27a7c7d5014d815
sabbir1257 <masabbir1257@gmail.com>
2023-02-13 03:13:35 +0600

    push

--- js/login.js
+++ js/login.js
@@ -8,5 +8,15 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     // 3.a: set id on the html
     // 3.b: get the element
     // 3.c: get the value from the element
+    const passwordFild = document.getElementById('user-password');
+    const password = passwordFild.value;
     
+    // DANGER : Do not verify email password on the client side
+    // step-4: verify email and password and check whether valid user or not
+    if(email === 'masabbir1257@gmail.com' && password === 'secret'){
+        window.location.href = 'bank.html';
+    }
+    else{
+        console.log('invalid user');
+    }
 })
